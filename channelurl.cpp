883f1a6168893db934330716e59d3b8c48b629fd
KindOne <localhost>
2016-08-20 17:42:05 -0400

    channelurl: add this.

---
+++ channelurl.cpp
@@ -0,0 +1,34 @@
+// Channel URL blocker.
+// So services on freenode will spam this if they set a URL.
+// See "/msg ChanServ help set URL"
+// :services. 328 KindOne #channel :http://channel.url.goes.here
+
+#include <znc/IRCNetwork.h>
+#include <znc/Modules.h>
+
+using std::vector;
+
+class CChannelURL : public CModule {
+public:
+	MODCONSTRUCTOR(CChannelURL) {
+	}
+
+	virtual ~CChannelURL() {
+	}
+
+	EModRet OnRaw(CString& sLine) {
+		if (sLine.Token(1) == "328") {
+			return HALT;
+		}
+		return CONTINUE;
+	}
+};
+
+
+template<> void TModInfo<CChannelURL>(CModInfo& Info) {
+//	Info.SetWikiPage("ChannelURL");
+	Info.SetHasArgs(false);
+	Info.AddType(CModInfo::NetworkModule);
+}
+
+NETWORKMODULEDEFS(CChannelURL, "Hide Channel URL (Numeric 328) from IRC clients.")
