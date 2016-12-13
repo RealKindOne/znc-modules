2b07e6493e6fdfb4a80773d092ec92ef6e31de19
KindOne <localhost>
2016-12-13 02:53:42 -0500

    roulette: add this.

---
+++ roulette.cpp
@@ -0,0 +1,42 @@
+// Fun game of roulette.
+// Must be OP for this to work.
+
+// RUN AT OWN RISK.
+
+// First attempt at rand(), I have no idea if thats the correct way, it seems to work.
+
+
+#include <znc/Chan.h>
+#include <znc/Modules.h>
+#include <znc/IRCNetwork.h>
+
+using std::vector;
+
+class Croulette : public CModule {
+
+public:
+	MODCONSTRUCTOR(Croulette) {}
+
+	virtual ~Croulette() {}
+
+	virtual EModRet OnChanMsg(CNick& Nick, CChan& Channel, CString& sMessage) override {
+		if ((sMessage.Token(0) == "!roulette") && (Channel.HasPerm(CChan::Op))) {
+
+			int x = rand()%6+1;
+
+			if (x == 4) {
+				PutIRC("KICK " + Channel.GetName() + " " + Nick.GetNick() + " :*BANG*");
+			}
+			else {
+				PutIRC("PRIVMSG " + Channel.GetName() + " :" + Nick.GetNick() + " *CLICK*");
+			}
+		}
+	}
+};
+
+template <>
+void TModInfo<Croulette>(CModInfo& Info) {
+
+}
+
+NETWORKMODULEDEFS(Croulette, "Russian Roulette.")
