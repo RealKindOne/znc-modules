f7dc3f315deda0a652487219cf2e944a57235709
RealKindOne <RealKindOne@users.noreply.github.com>
2025-08-06 13:35:22 -0400

    klined: modernize and show channels.

--- klined.cpp
+++ klined.cpp
@@ -1,27 +1,33 @@
 // Detect /klines.
 
-#include <znc/IRCNetwork.h>
 #include <znc/Chan.h>
+#include <znc/IRCNetwork.h>
 #include <znc/Modules.h>
 
 using std::vector;
 class Cklined : public CModule {
-public:
-	MODCONSTRUCTOR(Cklined) {
-	}
+  public:
+    MODCONSTRUCTOR(Cklined) {}
 
-	virtual ~Cklined() {
-	}
+    virtual ~Cklined() {}
 
-	void OnQuit(const CNick& Nick, const CString& sMessage, const vector<CChan*>& vChans) override {
-		if (sMessage.Token(0) == "K-Lined") {
-			PutModule("" + Nick.GetNick() + " (" + Nick.GetIdent() + "@" + Nick.GetHost()+ ")");
-		}
-	}
+    void OnQuitMessage(CQuitMessage& Message,
+                       const vector<CChan*>& vChans) override {
+        const CNick& Nick = Message.GetNick();
+        const CString sMessage = Message.GetReason();
+        if (sMessage.Token(0) == "K-Lined") {
+            VCString vsChans;
+            for (CChan* pChan : vChans) {
+                vsChans.push_back(pChan->GetName());
+            }
+            PutModule("" + Nick.GetNick() + " (" + Nick.GetIdent() + "@" + Nick.GetHost() + ") from channels: " +
+                      CString(", ").Join(vsChans.begin(), vsChans.end()));
+        }
+    }
 };
-template<> void TModInfo<Cklined>(CModInfo& Info) {
-//	Info.SetWikiPage("klined");
-	Info.SetHasArgs(false);
-	Info.AddType(CModInfo::NetworkModule);
+template <>
+void TModInfo<Cklined>(CModInfo& Info) {
+    //	Info.SetWikiPage("klined");
+    Info.SetHasArgs(false);
 }
 NETWORKMODULEDEFS(Cklined, "Watch for K-Lines.")
