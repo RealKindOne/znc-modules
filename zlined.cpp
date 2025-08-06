351fe7ea080b01c84695c7a9ba5105ae2920f5df
RealKindOne <RealKindOne@users.noreply.github.com>
2025-08-06 13:35:45 -0400

    zlined: modernize and show channels.

--- zlined.cpp
+++ zlined.cpp
@@ -1,27 +1,33 @@
 // Detect /zlines.
 
-#include <znc/IRCNetwork.h>
 #include <znc/Chan.h>
+#include <znc/IRCNetwork.h>
 #include <znc/Modules.h>
 
 using std::vector;
 class Czlined : public CModule {
-public:
-	MODCONSTRUCTOR(Czlined) {
-	}
+  public:
+    MODCONSTRUCTOR(Czlined) {}
 
-	virtual ~Czlined() {
-	}
+    virtual ~Czlined() {}
 
-	void OnQuit(const CNick& Nick, const CString& sMessage, const vector<CChan*>& vChans) override {
-		if (sMessage.Token(0) == "Z-Lined") {
-			PutModule("" + Nick.GetNick() + " (" + Nick.GetIdent() + "@" + Nick.GetHost()+ ")");
-		}
-	}
+    void OnQuitMessage(CQuitMessage& Message,
+                       const vector<CChan*>& vChans) override {
+        const CNick& Nick = Message.GetNick();
+        const CString sMessage = Message.GetReason();
+        if (sMessage.Token(0) == "Z-Lined") {
+            VCString vsChans;
+            for (CChan* pChan : vChans) {
+                vsChans.push_back(pChan->GetName());
+            }
+            PutModule("" + Nick.GetNick() + " (" + Nick.GetIdent() + "@" + Nick.GetHost() + ") from channels: " +
+                      CString(", ").Join(vsChans.begin(), vsChans.end()));
+        }
+    }
 };
-template<> void TModInfo<Czlined>(CModInfo& Info) {
-//	Info.SetWikiPage("zlined");
-	Info.SetHasArgs(false);
-	Info.AddType(CModInfo::NetworkModule);
+template <>
+void TModInfo<Czlined>(CModInfo& Info) {
+    //	Info.SetWikiPage("zlined");
+    Info.SetHasArgs(false);
 }
 NETWORKMODULEDEFS(Czlined, "Watch for Z-Lines.")
