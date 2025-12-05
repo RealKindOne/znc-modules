1a6104707eb4c1e9c66e921cafef07fb9061fd19
RealKindOne <RealKindOne@users.noreply.github.com>
2025-12-05 15:11:05 -0500

    roulette: clang-format

--- roulette.cpp
+++ roulette.cpp
@@ -2,38 +2,37 @@
 // Must be OP for this to work.
 
 #include <znc/Chan.h>
-#include <znc/Modules.h>
 #include <znc/IRCNetwork.h>
+#include <znc/Modules.h>
 
 using std::vector;
 
 class Croulette : public CModule {
+  public:
+    MODCONSTRUCTOR(Croulette) {}
 
-public:
-	MODCONSTRUCTOR(Croulette) {}
+    virtual ~Croulette() {}
 
-	virtual ~Croulette() {}
+    virtual EModRet OnChanMsg(CNick& Nick, CChan& Channel, CString& sMessage) override {
+        if ((sMessage.Token(0).StripControls() == "!roulette") && (Channel.HasPerm(CChan::Op))) {
+            // Channel might be -n. Block outside users from playing.
+            if (Channel.FindNick(Nick.GetNick()) == nullptr) {
+                return HALT;
+            }
+            int x = rand() % 6 + 1;
 
-	virtual EModRet OnChanMsg(CNick& Nick, CChan& Channel, CString& sMessage) override {
-		if ((sMessage.Token(0).StripControls() == "!roulette") && (Channel.HasPerm(CChan::Op))) {
-			// Channel might be -n. Block outside users from playing.
-			if (Channel.FindNick(Nick.GetNick())  ==  nullptr) { return HALT; }
-			int x = rand()%6+1;
-
-			if (x == 4) {
-				PutIRC("KICK " + Channel.GetName() + " " + Nick.GetNick() + " :*BANG*");
-			}
-			else {
-				PutIRC("PRIVMSG " + Channel.GetName() + " :" + Nick.GetNick() + " *CLICK*");
-			}
-		}
-		return CONTINUE;
-	}
+            if (x == 4) {
+                PutIRC("KICK " + Channel.GetName() + " " + Nick.GetNick() + " :*BANG*");
+            } else {
+                PutIRC("PRIVMSG " + Channel.GetName() + " :" + Nick.GetNick() + " *CLICK*");
+            }
+        }
+        return CONTINUE;
+    }
 };
 
 template <>
 void TModInfo<Croulette>(CModInfo& Info) {
-
 }
 
 NETWORKMODULEDEFS(Croulette, "Russian Roulette.")
